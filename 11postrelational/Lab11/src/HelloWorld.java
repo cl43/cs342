3e9c16a376b5706dc542333bd2220ae59e80ca7f
cl43 <cl43@students.calvin.edu>
2017-04-28 23:05:25 -0400

    Add files via upload
    Completed Homework 11

--- 11postrelational/Lab11/src/HelloWorld.java
+++ 11postrelational/Lab11/src/HelloWorld.java
@@ -1,15 +1,44 @@
 /**
  * Created by cl43 on 4/21/2017.
  */
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
 // The Java class will be hosted at the URI path "/hello"
 @Path("/hello")
 public class HelloWorld {
     // The Java method will process HTTP GET requests
+    @Path("/api")
     @GET
+    @Produces("text/plain")
+    public String Get() {
+        return "Getting...";
+    }
+
+    @Path("/api/{put}")
+    @PUT
+    @Produces("text/plain")
+    public String Put(@PathParam("put") Integer x) {
+        return "Putting: " + x;
+    }
+
+    @Path("/api/{post}")
+    @POST
+    @Produces("text/plain")
+    public String Post(@PathParam("post") String s) {
+        return "Putting: " + s;
+    }
+
+    @Path("/api/{delete}")
+    @DELETE
+    @Produces("text/plain")
+    public String Delete(@PathParam("delete") Integer x) {
+        if(x < 0 || x > 9) {
+            return "Insert an integer between 0 and 9!";
+        }else{
+            return "Deleting: " + x;
+        }
+    }
+
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
     public String getClichedMessage() {
